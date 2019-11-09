c4d33fdbe53144a99360d4503b4a19556337df90
yidan yuan <yidanyuan@berkeley.edu>
2019-11-08 16:25:04 -0800

    yy - added create to feedback controller as well as corresponding tests

--- app/controllers/api/feedbacks_controller.rb
+++ app/controllers/api/feedbacks_controller.rb
@@ -1,7 +1,15 @@
 module Api
   class FeedbacksController < ApplicationController
     def create
-     # Implement your create action
+      @feedback = Feedback.new(feedback_params)
+      @feedback.valid?
+      render json: { success: false, error: @feedback.errors.messages }, status: :unprocessable_entity unless @feedback.save
+    end
+
+    private
+
+    def feedback_params
+      params.require(:feedback).permit(:user_name, :comment)
     end
   end
 end
